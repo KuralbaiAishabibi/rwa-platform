6791db7d848635ebcce68b5553eb06093e57e7db
Ленара Сымбаткызы <symbatkyzylenara@gmail.com>
2026-05-13 16:37:06 +0500

    fix: apply forge fmt and remove submodule fetching from CI

--- script/DeployGovernance.s.sol
+++ script/DeployGovernance.s.sol
@@ -10,31 +10,24 @@ contract DeployGovernance is Script {
     function run() external {
         uint256 pk = vm.envUint("PRIVATE_KEY");
         address deployer = vm.addr(pk);
-        
+
         vm.startBroadcast(pk);
-        
+
         GovernanceToken token = new GovernanceToken(deployer, 1_000_000e18);
-        
+
         address[] memory proposers = new address[](1);
         proposers[0] = deployer;
         address[] memory executors = new address[](1);
         executors[0] = deployer;
-        
+
         RWATimelock timelock = new RWATimelock(172800, proposers, executors, deployer);
-        
-        RWAGovernor governor = new RWAGovernor(
-            token,
-            timelock,
-            6570,
-            46000,
-            10_000e18,
-            4
-        );
-        
+
+        RWAGovernor governor = new RWAGovernor(token, timelock, 6570, 46000, 10_000e18, 4);
+
         token.transferOwnership(address(timelock));
-        
+
         vm.stopBroadcast();
-        
+
         console.log("=== Governance Deployed ===");
         console.log("Token:", address(token));
         console.log("Timelock:", address(timelock));
